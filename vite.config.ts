2b99dc00c622e01abceccbe9dc343bf6fd7883bb
beneditojuniorbr <beneditosouzajr@gmail.com>
2026-04-09 09:01:03 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // Needed for mobile testing on same network
+    port: 3000
+  }
+})
